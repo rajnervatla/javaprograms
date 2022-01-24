2fd0dabd7ad6acf4d65237296e6fc71b3df63399
NERVATLA <rajkumar.nervatla@capgemini.com>
2022-01-24 17:54:17 +0530

    Deleted the sysout lines

--- src/programs/GitHubTesting.java
+++ src/programs/GitHubTesting.java
@@ -5,8 +5,7 @@ public class GitHubTesting {
 	public static void main(String[] args) {
 		System.out.println("Pushing the code into GitHUb location....");
 		System.out.println("Modifying the code in git hub repository");
-		System.out.println("Editing the code in local");
-		System.out.println("Editing in the feature branch raj Git practice");
+		
 		System.out.println("This is the feature branch testing from the local....");
 		
 		System.out.println("Pushing it to other remote branch for Feature branch testing on 17:50 pm");
