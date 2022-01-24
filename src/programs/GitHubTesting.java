ec717948188441251621df968d5184d0161bc50c
NERVATLA <rajkumar.nervatla@capgemini.com>
2022-01-24 23:02:05 +0530

    Testing the git

--- src/programs/GitHubTesting.java
+++ src/programs/GitHubTesting.java
@@ -6,6 +6,8 @@ public class GitHubTesting {
 		System.out.println("Pushing the code into GitHUb location....");
 		System.out.println("Modifying the code in git hub repository");
 		System.out.println("Editing the code in local");
+		
+		System.out.println("Commiting the code in to the remote branch...");
 	}
 
 }
