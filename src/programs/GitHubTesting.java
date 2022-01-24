205fd81382ded04f1c115167f351285c7d1de07b
NERVATLA <rajkumar.nervatla@capgemini.com>
2022-01-24 17:19:58 +0530

    I am commiting from feature branch to remote for merge testing

--- src/programs/GitHubTesting.java
+++ src/programs/GitHubTesting.java
@@ -6,6 +6,8 @@ public class GitHubTesting {
 		System.out.println("Pushing the code into GitHUb location....");
 		System.out.println("Modifying the code in git hub repository");
 		System.out.println("Editing the code in local");
+		System.out.println("Editing in the feature branch raj Git practice");
+		System.out.println("This is the feature branch testing from the local....");
 	}
 
 }
